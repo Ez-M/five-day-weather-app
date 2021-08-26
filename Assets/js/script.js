4656f64cc0cb92fea02be39df215a9f9c0a65c6a
Ezequiel <martinez.ezequiel42@gmail.com>
2021-08-26 01:28:29 -0700

    Added separate function to populate main card

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -18,9 +18,14 @@ $("ul").append("<a href=#>"+searchIn+"<a>");
 
 // }
 
+
+
+
+
+
 //Function that sends user input to openWeather API and receives data
 function search() {
-    dayUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchIn + "&appid=24d006899a454dfd2614392530a22f1a"
+    dayUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchIn + "&units=imperial&appid=24d006899a454dfd2614392530a22f1a"
     fetch(dayUrl, {
 
         method: 'get', //get is the default
@@ -38,14 +43,35 @@ function search() {
         .then(function (data) {
             console.log(data);
             console.log(data.main.temp);
-
-            $("#cTemp").text('Current Temp: '+data.main.temp);
-            $("#minTemp").text("Minimum Temp: "+data.main.temp_min);
-            $("#maxTemp").text("Maximum Temp: "+data.main.temp_max);
+            updateMain(data);
+            // $("#cTemp").text('Current Temp: '+data.main.temp);
+            // $("#minTemp").text("Minimum Temp: "+data.main.temp_min);
+            // $("#maxTemp").text("Maximum Temp: "+data.main.temp_max);
+            // $("#wind").text("Wind: "+data.wind.speed+ "MPH");
+            // $("#humidity").text("Humidity: "+data.main.humidity);
+            // $("#UV").text("UV Index: "+data.main.temp_max);
 
         });
 
 }
+
+
+
+
+/*This function handles updating each of the text elements in the
+main forcast card */
+function updateMain(data) {
+    $("#nameMain").text(data.name);
+    $("#cTemp").text('Current Temp: '+data.main.temp);
+    $("#minTemp").text("Minimum Temp: "+data.main.temp_min);
+    $("#maxTemp").text("Maximum Temp: "+data.main.temp_max);
+    $("#wind").text("Wind: "+data.wind.speed+ "MPH");
+    $("#humidity").text("Humidity: "+data.main.humidity);
+    $("#UV").text("UV Index: "+data.main.temp_max);
+
+
+}
+
 
 
 
